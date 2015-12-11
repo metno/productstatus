4d58bea5eafe74846a246b82c13826ea9f06db1f
Kim Tore Jensen <kimtj@met.no>
2015-12-11 13:32:33 +0100

    Make zmq_test_subscriber prettier
    Summary: This patch prettifies zmq_test_subscriber, for demo purposes. It also adds the possibility to specify a custom remote endpoint.
    
    Test Plan: Run `contrib/zmq_test_subscriber.py`.
    
    Reviewers: #ecdiss, havardf
    
    Reviewed By: #ecdiss, havardf
    
    Projects: #ecdiss
    
    Differential Revision: https://phab.met.no/D1289

--- contrib/zmq_test_subscriber.py
+++ contrib/zmq_test_subscriber.py
@@ -1,11 +1,29 @@
+#!/usr/bin/env python2.7
+
+import datetime
+import argparse
 import zmq
+import pprint
+
+parser = argparse.ArgumentParser()
+parser.add_argument('socket', action='store', default='tcp://127.0.0.1:9797', nargs='?')
+args = parser.parse_args()
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:9797")
+socket.connect(args.socket)
 socket.setsockopt(zmq.SUBSCRIBE, '')
 
+print '********************'
+print 'Listening for events'
+print 'Remote:', args.socket
+print '********************'
+
+events = 0
+
 while True:
-    print "Listening..."
     msg = socket.recv_json()
-    print "Productstatus msg:%s" % (msg)
+    events += 1
+    print
+    print '---', 'Event', events, 'received on', datetime.datetime.now()
+    pprint.pprint(msg)

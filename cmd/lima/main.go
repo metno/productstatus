57ae6f2524a7938afbde57a913d5267f1b070b59
Kim Tore Jensen <kimtj@met.no>
2017-09-01 10:37:30 +0200

    Support command-line flags and torrent loading

--- cmd/lima/main.go
+++ cmd/lima/main.go
@@ -1,17 +1,49 @@
 package main
 
-import "github.com/anacrolix/torrent"
+import (
+	"fmt"
 
-func newConfig() *torrent.Config {
-	c := &torrent.Config{}
+	"github.com/anacrolix/torrent"
+
+	flags "github.com/jessevdk/go-flags"
+)
+
+type cliOptions struct {
+	DataDir    string   `long:"datadir" description:"Where to read and write data" required:"true"`
+	ListenAddr string   `long:"listen" description:"Listen address" value-name:"HOST:PORT" required:"true"`
+	Peers      []string `long:"peers" description:"Peers to connect to" value-name:"HOST:PORT" required:"true"`
+	Torrent    string   `long:"torrent" description:"Torrent file to load" required:"true"`
+}
+
+func newConfig(opts cliOptions) *torrent.Config {
+	c := &torrent.Config{
+		DataDir:           opts.DataDir,
+		DisableEncryption: true,
+		DisableTrackers:   true,
+	}
 	return c
 }
 
 func main() {
-	cfg := newConfig()
+	var opts cliOptions
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := newConfig(opts)
 	cl, err := torrent.NewClient(cfg)
 	if err != nil {
 		panic(err)
 	}
+
+	torrent, err := cl.AddTorrentFromFile(opts.Torrent)
+	if err != nil {
+		panic(err)
+	}
+
+	infohash := torrent.InfoHash()
+	fmt.Printf("Loaded torrent %s with info hash %v\n", torrent.Name(), infohash)
+
 	cl.Close()
 }
